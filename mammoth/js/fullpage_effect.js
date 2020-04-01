b4b169a55e32b3b9f970c54a85d3a313a68d3547
myid-sysy <60874174+myid-sysy@users.noreply.github.com>
2020-04-01 14:04:47 +0900

    page 4 class change / page 5 publishing
    page 4 class change
    page 5 publishing

--- mammoth/js/fullpage_effect.js
+++ mammoth/js/fullpage_effect.js
@@ -4,7 +4,7 @@ $(document).ready(function(){
     verticalCentered: false,
     navigation: true,
     navigationPosition:'right',
-    anchors:['1st', '2nd' , '3rd' ,'4th'],
+    anchors:['1st', '2nd' , '3rd' ,'4th','5th'],
     afterLoad:function(anchorLink, index){
       // console.log('anchorLink:' +anchorLink+'index:' +index)
       if(anchorLink == '2nd'){
@@ -14,9 +14,13 @@ $(document).ready(function(){
         $(".main_txt_3").addClass("txt");
         $(".page_logo_3").addClass("text_up");
       } else if(anchorLink == '4th'){
-        $(".page_title").addClass("slide_up_1");
-        $(".page_sub_title").addClass("slide_up_2")
+        $(".slide_title").addClass("slide_up_1");
+        $(".slide_sub_title").addClass("slide_up_2")
         $(".slides").addClass("slide_up_3")
+      } else if(anchorLink == '5th'){
+        $(".insta_title").addClass("slide_up_1");
+        $(".insta_sub_title").addClass("slide_up_2");
+        $(".slides").addClass("slide_up_3");
       }
     }
   });
